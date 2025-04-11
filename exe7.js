df9cf37fa247b13e60e8f7a35d14fde7ae73366d
Luiz Felipe <luizfelipeallagedd@gmail.com>
2025-04-11 11:43:59 -0300

    07

---
+++ exe7.js
@@ -0,0 +1,9 @@
+let total = 0;
+let moedas = 0;
+while (total < 12) {
+    moedas++;
+    total += 3;
+    console.log(`Passo ${moedas}: Total = ${total} reais`);
+
+}
+console.log(`Total de moedas: ${moedas}`);
